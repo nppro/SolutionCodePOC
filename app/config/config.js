564cad1a2d161fb881d4632f3001f2f69ccf814a
Phuoc Pham <phamnguyenphuoc@gmail.com>
2025-12-23 17:02:53 +0100

    Define variable

--- app/config/config.js
+++ app/config/config.js
@@ -1,62 +1,62 @@
 // define default config, but allow overrides from ENV vars
 let config = {
-  APP_DB_HOST: "",
-  APP_DB_USER: "",
-  APP_DB_PASSWORD: "",
-  APP_DB_NAME: ""
+  APP_DB_HOST: "localhost",
+  APP_DB_USER: "nodeapp",
+  APP_DB_PASSWORD: "student12",
+  APP_DB_NAME: "STUDENTS"
 }
 
-var AWS = require('aws-sdk');
+// var AWS = require('aws-sdk');
 
-try {
-var client = new AWS.SecretsManager({
-    region: "us-east-1"
-});
+// try {
+// var client = new AWS.SecretsManager({
+//     region: "us-east-1"
+// });
 
-const secretName = "Mydbsecret";
+// const secretName = "Mydbsecret";
 
 
-client.getSecretValue({SecretId: secretName}, function(err, data) {
-    if (err) {
-      config.APP_DB_HOST = "localhost"
-      config.APP_DB_NAME = "STUDENTS"
-      config.APP_DB_PASSWORD = "student12"
-      config.APP_DB_USER = "nodeapp"
-      console.log('Secrets not found. Proceeding with default values..')
-          //  throw err;
-    }
-    else {
-        if ('SecretString' in data) {
-            secret = JSON.parse(data.SecretString);
-            for(const envKey of Object.keys(secret)) {
-                process.env[envKey] = secret[envKey];
-              //  console.log(` Value for key '${envKey}' `);
-              //  console.log(` secret[envKey] '${secret[envKey]}'`);
-                if (envKey == 'user') {
-                  config.APP_DB_USER = secret[envKey]
-                } else if (envKey == 'password') {
-                  config.APP_DB_PASSWORD = secret[envKey]
-                } else if (envKey == 'host') {
-                  config.APP_DB_HOST = secret[envKey]
-                } else if (envKey == 'db') {
-                  config.APP_DB_NAME= secret[envKey]
-                }
-        }
+// client.getSecretValue({SecretId: secretName}, function(err, data) {
+//     if (err) {
+//       config.APP_DB_HOST = "localhost"
+//       config.APP_DB_NAME = "STUDENTS"
+//       config.APP_DB_PASSWORD = "student12"
+//       config.APP_DB_USER = "nodeapp"
+//       console.log('Secrets not found. Proceeding with default values..')
+//           //  throw err;
+//     }
+//     else {
+//         if ('SecretString' in data) {
+//             secret = JSON.parse(data.SecretString);
+//             for(const envKey of Object.keys(secret)) {
+//                 process.env[envKey] = secret[envKey];
+//               //  console.log(` Value for key '${envKey}' `);
+//               //  console.log(` secret[envKey] '${secret[envKey]}'`);
+//                 if (envKey == 'user') {
+//                   config.APP_DB_USER = secret[envKey]
+//                 } else if (envKey == 'password') {
+//                   config.APP_DB_PASSWORD = secret[envKey]
+//                 } else if (envKey == 'host') {
+//                   config.APP_DB_HOST = secret[envKey]
+//                 } else if (envKey == 'db') {
+//                   config.APP_DB_NAME= secret[envKey]
+//                 }
+//         }
 		
-        }
+//         }
 
-    }
-    // console log in case of error
-    //console.log(err);
-});
+//     }
+//     // console log in case of error
+//     //console.log(err);
+// });
 
-} catch (e) {
-  config.APP_DB_HOST = "localhost";
-  config.APP_DB_NAME = "STUDENTS";
-  config.APP_DB_PASSWORD = "student12";
-  config.APP_DB_USER = "nodeapp";
-  console.log('Secrets not found. Proceeding with default values..');
-}
+// } catch (e) {
+//   config.APP_DB_HOST = "localhost";
+//   config.APP_DB_NAME = "STUDENTS";
+//   config.APP_DB_PASSWORD = "student12";
+//   config.APP_DB_USER = "nodeapp";
+//   console.log('Secrets not found. Proceeding with default values..');
+// }
 
 
 
