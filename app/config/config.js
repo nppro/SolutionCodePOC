86f8ebcb6b031e9faa5aec789ca18a44639e064b
Phuoc Pham <phamnguyenphuoc@gmail.com>
2026-01-12 22:54:35 +0100

    Set up phase 4 to connect RDS and deploy EC2

--- app/config/config.js
+++ app/config/config.js
@@ -3,68 +3,53 @@ let config = {
   APP_DB_HOST: "localhost",
   APP_DB_USER: "nodeapp",
   APP_DB_PASSWORD: "student12",
-  APP_DB_NAME: "STUDENTS"
+  APP_DB_NAME: "STUDENTS",
+};
+
+var AWS = require("@aws-sdk/client-secrets-manager");
+
+try {
+  var client = new AWS.SecretsManagerClient({
+    region: "us-east-1",
+  });
+
+  const secretName = "prod/WebApp/Mysql";
+
+  client
+    .send(
+      new GetSecretValueCommand({
+        SecretId: secretName,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    )
+    .then((data) => {
+      const secret = data.SecretString;
+      const parsedSecret = JSON.parse(secret);
+
+      config.APP_DB_HOST = parsedSecret.host;
+      config.APP_DB_NAME = parsedSecret.db;
+      config.APP_DB_PASSWORD = parsedSecret.password;
+      config.APP_DB_USER = parsedSecret.user;
+    })
+    .catch((err) => {
+      console.log("Error retrieving secrets from AWS Secrets Manager", err);
+      throw err;
+    });
+} catch (e) {
+  config.APP_DB_HOST = "localhost";
+  config.APP_DB_NAME = "STUDENTS";
+  config.APP_DB_PASSWORD = "student12";
+  config.APP_DB_USER = "nodeapp";
+  console.log("Secrets not found. Proceeding with default values..");
 }
 
-// var AWS = require('aws-sdk');
-
-// try {
-// var client = new AWS.SecretsManager({
-//     region: "us-east-1"
-// });
-
-// const secretName = "Mydbsecret";
-
-
-// client.getSecretValue({SecretId: secretName}, function(err, data) {
-//     if (err) {
-//       config.APP_DB_HOST = "localhost"
-//       config.APP_DB_NAME = "STUDENTS"
-//       config.APP_DB_PASSWORD = "student12"
-//       config.APP_DB_USER = "nodeapp"
-//       console.log('Secrets not found. Proceeding with default values..')
-//           //  throw err;
-//     }
-//     else {
-//         if ('SecretString' in data) {
-//             secret = JSON.parse(data.SecretString);
-//             for(const envKey of Object.keys(secret)) {
-//                 process.env[envKey] = secret[envKey];
-//               //  console.log(` Value for key '${envKey}' `);
-//               //  console.log(` secret[envKey] '${secret[envKey]}'`);
-//                 if (envKey == 'user') {
-//                   config.APP_DB_USER = secret[envKey]
-//                 } else if (envKey == 'password') {
-//                   config.APP_DB_PASSWORD = secret[envKey]
-//                 } else if (envKey == 'host') {
-//                   config.APP_DB_HOST = secret[envKey]
-//                 } else if (envKey == 'db') {
-//                   config.APP_DB_NAME= secret[envKey]
-//                 }
-//         }
-		
-//         }
-
-//     }
-//     // console log in case of error
-//     //console.log(err);
-// });
-
-// } catch (e) {
-//   config.APP_DB_HOST = "localhost";
-//   config.APP_DB_NAME = "STUDENTS";
-//   config.APP_DB_PASSWORD = "student12";
-//   config.APP_DB_USER = "nodeapp";
-//   console.log('Secrets not found. Proceeding with default values..');
-// }
-
-
-
-Object.keys(config).forEach(key => {
-  if(process.env[key] === undefined){
-    console.log(`[NOTICE] Value for key '${key}' not found in ENV, using default value.  See app/config/config.js`)
+Object.keys(config).forEach((key) => {
+  if (process.env[key] === undefined) {
+    console.log(
+      `[NOTICE] Value for key '${key}' not found in ENV, using default value.  See app/config/config.js`
+    );
   } else {
-    config[key] = process.env[key]
+    config[key] = process.env[key];
   }
 });
 
